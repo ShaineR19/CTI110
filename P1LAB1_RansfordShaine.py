9c098bd85ed18d7aef6799074f1c589eb84e9c0a
ShaineR19 <159803141+ShaineR19@users.noreply.github.com>
2024-02-12 11:06:04 -0500

    Add files via upload

---
+++ P1LAB1_RansfordShaine.py
@@ -0,0 +1,11 @@
+# Shaine Ransford
+# 02/12/2024
+# OP1LAB1
+# This program ask for user input then welcomes them to zybooks.
+
+# Create 
+user_name = input("Enter First Name:")
+
+# Print
+print("Hey", user_name)
+print("Welcome to zyBooks!")
