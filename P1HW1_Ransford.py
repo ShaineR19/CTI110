fbf04b4fd99ad1455d320e2de9e021472c6ea7d3
ShaineR19 <159803141+ShaineR19@users.noreply.github.com>
2024-02-14 10:46:10 -0500

    Add files via upload

---
+++ P1HW1_Ransford.py
@@ -0,0 +1,21 @@
+# Shaine Ransford
+# 02/14/2024
+# P1HW1
+# The user enters two numbers and those number are used in calculations
+
+num = int(input("Enter integer: "))
+
+print("You entered:", num)
+print(num, "squared is", num * num)
+print("And", num ,"cubed is", num * num * num, "!!")
+
+num2 = int(input("Enter another integer: "))
+
+answer = num + num2;
+answer2 = num * num2;
+
+print("You entered:", num2)
+print(num ,"+", num2 ,"is", answer)
+
+print(num ,"*", num2 ,"is", answer2)
+
